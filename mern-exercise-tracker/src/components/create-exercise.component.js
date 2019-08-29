c91c151b54e3e46b28ac47c49db6d7e0260282bb
Breyanna Vass <breyannavass@gmail.com>
2019-08-29 14:53:30 -0400

    adding basic functionality

---
+++ mern-exercise-tracker/src/components/create-exercise.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CreateExercise extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
